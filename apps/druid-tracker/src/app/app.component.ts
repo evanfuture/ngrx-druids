ccdca7cb1dc3be7057916079b7535d425075a416
evanfuture <evan@evanpayne.com>
2021-09-15 19:05:19 +0200

    initial commit

---
+++ apps/druid-tracker/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngrx-druids-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'druid-tracker';
+}
